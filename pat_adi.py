f92eca147bfb15b68d7516ef244e02cf8c9bf207
aditya7223 <88703873+aditya7223@users.noreply.github.com>
2022-06-09 18:11:54 +0530

    Update pat_adi.py

--- pat_adi.py
+++ pat_adi.py
@@ -3,16 +3,11 @@ import boto3
 import json
 import time
 def main():
-    start_time = time.time()
-    #return("Start Time of Main --- %s seconds ---" % (start_time))
     num1 = 5
     num2 = 6
-     
+ 
     # Add two numbers
     sum = float(num1) + float(num2)
-     
+ 
     # Display the sum
-    end_time = time.time()
-    return ('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-    
-    #return("Read the file from S3 at", end_time , "and time elapsed is  --> ", (end_time - start_time) )
+    return('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
