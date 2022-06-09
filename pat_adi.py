3467483b6482a2b475042931ad809ac348b5303f
aditya7223 <88703873+aditya7223@users.noreply.github.com>
2022-06-09 17:19:14 +0530

    Update pat_adi.py

--- pat_adi.py
+++ pat_adi.py
@@ -1,2 +1,9 @@
 def main():
-    return "hello,i am aditya"
+    num1 = 5
+    num2 = 6
+     
+    # Add two numbers
+    sum = float(num1) + float(num2)
+     
+    # Display the sum
+    return('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
