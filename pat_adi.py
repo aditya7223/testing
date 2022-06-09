bbb4a1089c73f464fb34f03525b097893f4b6018
aditya7223 <88703873+aditya7223@users.noreply.github.com>
2022-06-09 17:42:00 +0530

    Update pat_adi.py

--- pat_adi.py
+++ pat_adi.py
@@ -1,7 +1,7 @@
 import time
 def main():
     start_time = time.time()
-    # print("Start Time of Main --- %s seconds ---" % (start_time))
+    return("Start Time of Main --- %s seconds ---" % (start_time))
     num1 = 5
     num2 = 6
      
@@ -9,6 +9,6 @@ def main():
     sum = float(num1) + float(num2)
      
     # Display the sum
-    return('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+    #return('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
     end_time = time.time()
     #return("Read the file from S3 at", end_time , "and time elapsed is  --> ", (end_time - start_time) )
