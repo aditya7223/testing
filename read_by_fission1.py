5a43a5a437ac25539c114f8738a3400cde8e023c
aditya7223 <88703873+aditya7223@users.noreply.github.com>
2022-06-13 21:30:44 +0530

    Add files via upload

---
+++ read_by_fission1.py
@@ -0,0 +1,33 @@
+import sys
+import boto3
+import json
+import time
+
+def main():
+	# start_time = time.time()
+	# print("Start Time of Main --- %s seconds ---" % (start_time))
+	session = boto3.Session(
+			    aws_access_key_id='AKIAXBNIAY3OSGPM5GHY',
+			    aws_secret_access_key='sP4JthlrxMA1ntR4EwQfEJhNuHZ5xyZh/0j61onQ',
+			    region_name='us-east-1')
+			    
+	s3 = session.resource('s3')
+
+	# get a handle on the bucket that holds your file
+	bucket = s3.Bucket('test-case-1') # example: energy_market_procesing
+
+	# get a handle on the object you want (i.e. your file)
+	obj = bucket.Object(key='2.json') # example: market/zone1/data.csv
+
+	# get the object
+	response = obj.get()
+
+	# read the contents of the file
+	lines = response['Body'].read().decode()
+	data = json.loads(lines)
+	transactions = data['users']
+	#transactions = data['dataset']['fields']
+	print(str(len(transactions)))
+	# end_time = time.time()
+	# print("Read the file from S3 at", end_time , "and time elapsed is  --> ", (end_time - start_time) )
+	return str(len(transactions))
