79cff12b02245eca07c2009b241b8ae4f4040c6d
aditya7223 <88703873+aditya7223@users.noreply.github.com>
2022-06-13 21:41:24 +0530

    Add files via upload

---
+++ read_by_fission2.py
@@ -0,0 +1,13 @@
+import sys
+import boto3
+import json
+import time
+
+def main():
+	# start_time = time.time()
+	# print("Start Time of Main --- %s seconds ---" % (start_time))
+	session = boto3.Session(
+			    aws_access_key_id='AKIA2OK6XUCFETUVJ2L6',
+			    aws_secret_access_key='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',
+			    region_name='us-east-1')
+	return "\n"
