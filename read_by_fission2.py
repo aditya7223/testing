2fca5bf05c35fe3520fa18b3974e4de818da49fd
aditya7223 <88703873+aditya7223@users.noreply.github.com>
2022-06-13 21:42:27 +0530

    Update read_by_fission2.py

--- read_by_fission2.py
+++ read_by_fission2.py
@@ -7,7 +7,7 @@ def main():
 	# start_time = time.time()
 	# print("Start Time of Main --- %s seconds ---" % (start_time))
 	session = boto3.Session(
-			    aws_access_key_id='AKIA2OK6XUCFETUVJ2L6',
-			    aws_secret_access_key='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',
+			    aws_access_key_id='AKIAXBNIAY3OSGPM5GHY',
+			    aws_secret_access_key='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',
 			    region_name='us-east-1')
 	return "\n"
