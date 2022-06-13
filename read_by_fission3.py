012c4640f9966096210bc688442a191deb33d728
aditya7223 <88703873+aditya7223@users.noreply.github.com>
2022-06-13 22:48:21 +0530

    Update read_by_fission3.py

--- read_by_fission3.py
+++ read_by_fission3.py
@@ -4,5 +4,5 @@ import json
 import time
 
 def main():
-	#session = boto3.Session(aws_access_key_id='AKIAXBNIAY3OSGPM5GHY',aws_secret_access_key='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',region_name='us-east-1')
+	session = boto3.Session(aws_access_key_id='AKIAXBNIAY3OSGPM5GHY',aws_secret_access_key='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',region_name='us-east-1')
 	return "\n"
